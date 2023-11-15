18c03b0a63ef0bf6f95f5440e863941168afa6b5
Sebastian Noe <sebastian.schneider@boxine.de>
2023-11-15 15:49:58 +0100

    save details and skip a redundant request

--- send_email.py
+++ send_email.py
@@ -25,20 +25,20 @@ tvdb = tvdb_v4_official.TVDB(THE_TVDB_API_KEY)
 
 favorites = tvdb.get_user_favorites()['series']
 airs_next = []
+series_details = {}
 for series in favorites:
-    series_details = tvdb.get_series_extended(id=series)
-    print(f'retrieving details for {series_details["name"]}')
+    series_details[series] = tvdb.get_series_extended(id=series)
+    print(f'retrieving details for {series_details[series]["name"]} , Series ID: {series}')
     details = tvdb.get_series_nextAired(id=series)
     if details['nextAired'] != '':
         nextAired = datetime.date.fromisoformat(details['nextAired'])
         today = datetime.date.today()
         if today == nextAired:
-            print(f'Today ({today.strftime('%d.%m.%y')}) airs: {series_details["name"]}')
+            print(f'Today ({today.strftime("%d/%m/%y")}) airs: {series_details[series]["name"]}')
             airs_next.append(series)
 
 for serie in airs_next:
     episodes = tvdb.get_series_episodes(id=serie)
-    series_details = tvdb.get_series_extended(id=series)
     new_episodes_list = []
     new_episodes_name = {}
     for episode in episodes.get('episodes',[]):
@@ -48,11 +48,11 @@ for serie in airs_next:
                 new_episodes_name[f'{episode["number"]}'] = episode['name']
     episode_date = today.strftime("%A, %d. %B")
     try:
-        artwork_url = series_details['artworks'][0]['image']
+        artwork_url = series_details[serie]['artworks'][0]['image']
     except:
         artwork_url = ''
-    series_name = series_details['name']
-    series_url = 'https://thetvdb.com/series/' + series_details['slug']
+    series_name = series_details[serie]['name']
+    series_url = 'https://thetvdb.com/series/' + series_details[serie]['slug']
     if len(new_episodes_list) > 1:
             episode_name = ''
             episode_no = f'{new_episodes_list[0]}-{new_episodes_list[len(new_episodes_list)-1]}'
@@ -77,7 +77,7 @@ for serie in airs_next:
     msg['From'] = f'Series Reminder Bot <{SMTP_USER}>'
     msg['to'] = RECEIVER_EMAIL
     msg.set_content(email_content, subtype='html')
-    print(f'Sending email about new episodes for "{series_details["name"]}"')
+    print(f'Sending email about new episodes for "{series_details[serie]["name"]}"')
     with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_SSL_PORT, context=context) as server:
         server.login(SMTP_USER, SMTP_PASSWORD)
         server.send_message(msg)
