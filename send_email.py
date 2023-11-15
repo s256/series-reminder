e1d54c17a08c8ca078206bd45fdbf61e9cbd625d
Sebastian Noe <sebastian.schneider@boxine.de>
2023-11-15 14:12:10 +0100

    fix indentation

--- send_email.py
+++ send_email.py
@@ -57,9 +57,9 @@ for serie in airs_next:
             episode_name = ''
             episode_no = f'{new_episodes_list[0]}-{new_episodes_list[len(new_episodes_list)-1]}'
             for index, episode in enumerate(new_episodes_list):
-            episode_name += new_episodes_name[f'{episode}']
-            if not index == len(new_episodes_list) -1:
-                episode_name += ', '
+                episode_name += new_episodes_name[f'{episode}']
+                if not index == len(new_episodes_list) -1:
+                    episode_name += ', '
     else:
         episode_no = episode['number']
         episode_name = episode['name']
